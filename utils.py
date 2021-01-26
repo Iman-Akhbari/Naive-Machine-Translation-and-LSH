b16a8243f2c1a76a484ec3aac53703f69b1fafea
Iman Akhbari <59622942+Iman-Akhbari@users.noreply.github.com>
2021-01-26 13:31:47 -0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,70 +1,21 @@
-import re
-import string
-
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import TweetTokenizer
+import numpy as np
 
 
-def process_tweet(tweet):
-    '''
+def get_vectors(embeddings, words):
+    """
     Input:
-        tweet: a string containing a tweet
-    Output:
-        tweets_clean: a list of words containing the processed tweet
-
-    '''
-    stemmer = PorterStemmer()
-    stopwords_english = stopwords.words('english')
-    # remove stock market tickers like $GE
-    tweet = re.sub(r'\$\w*', '', tweet)
-    # remove old style retweet text "RT"
-    tweet = re.sub(r'^RT[\s]+', '', tweet)
-    # remove hyperlinks
-    tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
-    # remove hashtags
-    # only removing the hash # sign from the word
-    tweet = re.sub(r'#', '', tweet)
-    # tokenize tweets
-    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
-                               reduce_len=True)
-    tweet_tokens = tokenizer.tokenize(tweet)
-
-    tweets_clean = []
-    for word in tweet_tokens:
-        if (word not in stopwords_english and  # remove stopwords
-            word not in string.punctuation):  # remove punctuation
-            # tweets_clean.append(word)
-            stem_word = stemmer.stem(word)  # stemming word
-            tweets_clean.append(stem_word)
-
-    return tweets_clean
-
-
-def test_lookup(func):
-    freqs = {('sad', 0): 4,
-             ('happy', 1): 12,
-             ('oppressed', 0): 7}
-    word = 'happy'
-    label = 1
-    if func(freqs, word, label) == 12:
-        return 'SUCCESS!!'
-    return 'Failed Sanity Check!'
-
-
-def lookup(freqs, word, label):
-    '''
-    Input:
-        freqs: a dictionary with the frequency of each pair (or tuple)
-        word: the word to look up
-        label: the label corresponding to the word
-    Output:
-        n: the number of times the word with its corresponding label appears.
-    '''
-    n = 0  # freqs.get((word, label), 0)
-
-    pair = (word, label)
-    if (pair in freqs):
-        n = freqs[pair]
-
-    return n
+        embeddings: a word 
+        fr_embeddings:
+        words: a list of words
+    Output: 
+        X: a matrix where the rows are the embeddings corresponding to the rows on the list
+        
+    """
+    m = len(words)
+    X = np.zeros((1, 300))
+    for word in words:
+        english = word
+        eng_emb = embeddings[english]
+        X = np.row_stack((X, eng_emb))
+    X = X[1:,:]
+    return X
